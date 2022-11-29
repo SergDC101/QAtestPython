f9be1e8d94cc00d6c92c6fee8b5722290b3d39a1
Sergey <sergdc101@gmail.com>
2022-11-29 16:44:23 +0300

    second commit

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,8 +1,16 @@
 import pytest
-from selenium import webdriver
+
+from pom.homepage_nav import HomePageNav
 
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
-    pass
+
+    def test_nav_links(self):
+        homepage_nav = HomePageNav(self.driver)
+        actual_links = homepage_nav.get_nav_links_text()
+        expected_links = homepage_nav.NAV_LINK_TEXT
+        assert expected_links == actual_links, 'Validating Nav Links text'
+
+
 
